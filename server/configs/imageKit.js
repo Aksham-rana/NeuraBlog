7815efed6c05c5638f25d56b7d1a34b4814ff5dc
Aksham Rana <ranaaksham2611@gmail.com>
2026-02-18 12:01:40 +0530

    Fix ImageKit crash on Railway

--- server/configs/imageKit.js
+++ server/configs/imageKit.js
@@ -1,9 +1,10 @@
-import ImageKit from 'imagekit'
+import ImageKit from 'imagekit';
+import 'dotenv/config';   // ✅ THIS LINE FIXES IT
 
-var imageKit = new ImageKit({
-    publicKey : process.env.IMAGEKIT_PUBLIC_KEY,
-    privateKey : process.env.IMAGEKIT_PRIVATE_KEY,
-    urlEndpoint : process.env.IMAGEKIT_URL_ENDPOINT
-})
+const imageKit = new ImageKit({
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY || "dummy",
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY || "dummy",
+    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT || "https://ik.imagekit.io/dummy"
+});
 
 export default imageKit;
