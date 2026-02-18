7fb7fcf47c03cd9ffa958441233582283277b880
Aksham Rana <ranaaksham2611@gmail.com>
2026-02-18 12:16:02 +0530

    Convert server from serverless to Railway server

--- server/server.js
+++ server/server.js
@@ -1,16 +1,15 @@
 import express from "express";
 import cors from "cors";
-import serverless from "serverless-http";
 import connectDB from "./configs/db.js";
 import adminRouter from "./routes/adminRoutes.js";
 import blogRouter from "./routes/blogRoutes.js";
 
 const app = express();
 
-// Connect database
+// connect database
 await connectDB();
 
-// Middleware
+// middleware
 app.use(cors({
   origin: "https://neura-blog-mu.vercel.app",
   credentials: true
@@ -19,14 +18,18 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Test route
+// test route
 app.get("/", (req, res) => {
   res.status(200).send("API is Working");
 });
 
-// Routes
+// routes
 app.use("/api/admin", adminRouter);
 app.use("/api/blog", blogRouter);
 
-// Export handler
-export default serverless(app);
+// IMPORTANT FOR RAILWAY
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
